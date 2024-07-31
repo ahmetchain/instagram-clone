6d1273d7611a1c7fd3e92b539f0afa4566d8c91f
Ahmet <ahmetarapaslan@gmail.com>
2024-07-31 20:44:05 +0300

    fix: inbox page

--- src/pages/inbox/inbox.js
+++ src/pages/inbox/inbox.js
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import Icon from "component/Icon";
+import React from "react";
+import InboxMessage from "Icon/inbox.svg";
 export default function Inbox() {
   return (
-    <div>inbox</div>
-  )
+    <div className="flex-1 flex ">
+      <div className="w-[400px] border-r border-gray-300  h-full">
+        <h1>Messages</h1>
+        <h1>Mesaaa</h1>
+      </div>
+      <div className=" h-full flex-1">
+        <div className="flex flex-col items-center justify-center h-full">
+          <div>
+            <Icon name={InboxMessage} size={110} />
+          </div>
+          <h2 className="text-xl font-semibold mt-3">Mesajların</h2>
+          <p>Bir arkadaşına veya gruba gizli fotoğraflar ve mesajlar gönder</p>
+          <div >
+            <button className=" px-3.5 bg-[#0095f6] text-white font-semibold rounded-md py-2 mt-3">Mesaj Gönder</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/inbox/index.jsx
+++ src/pages/inbox/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function InboxLayout() {
   return (
-    <div>
-        <Outlet/>
+    <div className="flex border border-gray-300   h-[calc(100vh-141px)]">
+      <Outlet />
     </div>
-  )
+  );
 }
