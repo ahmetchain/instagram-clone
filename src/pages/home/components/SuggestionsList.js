1fd0b52f0b1fce8c82752c47a09c0427e5547783
Ahmet <ahmetarapaslan@gmail.com>
2024-08-09 20:30:56 +0300

    fix: suggestionslist design completed

--- src/pages/home/components/SuggestionsList.js
+++ src/pages/home/components/SuggestionsList.js
@@ -3,61 +3,73 @@ import ProfilePicture from "Icon/pp.jpg";
 export default function SuggestionsList() {
   return (
     <div className=" w-[320px] flex flex-col">
-      <header className=" flex items-center w-full">
+      <header className=" flex items-center w-full mb-5">
         <img width={44} className=" rounded-full" src={ProfilePicture} />
         <div className="flex flex-col ml-3">
           <h2 className="font-semibold">ozcaanahmet</h2>
           <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
         </div>
-        <a className=" mx-auto text-blue-500" href="#">Geçiş yap</a>
+        <a className=" ml-auto text-blue-500" href="#">
+          Geçiş yap
+        </a>
       </header>
-      <main>
-        <header>
-          <h2> Senin için önerilenler</h2>
-          <a href="#">Tümünü gör</a>
-        </header>
-        <div>
-          <img src={ProfilePicture} />
-          <div>
-            <h2>ozcaanahmet</h2>
-            <p>Ahmet Özcan</p>
+      <header className="flex justify-between mb-5">
+        <h2> Senin için önerilenler</h2>
+        <a href="#">Tümünü gör</a>
+      </header>
+      <main className="flex flex-col gap-y-5 mb-5">
+        <div className="flex items-center ">
+          <img width={44} className=" rounded-full" src={ProfilePicture} />
+          <div className=" ml-3">
+            <h2 className=" font-semibold">ozcaanahmet</h2>
+            <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
           </div>
-          <a href="#">Takip et</a>
+          <a className="ml-auto text-blue-500" href="#">
+            Takip et
+          </a>
         </div>
-        <div>
-          <img src={ProfilePicture} />
-          <div>
-            <h2>ozcaanahmet</h2>
-            <p>Ahmet Özcan</p>
+        <div className="flex items-center ">
+          <img width={44} className=" rounded-full" src={ProfilePicture} />
+          <div className=" ml-3">
+            <h2 className=" font-semibold">ozcaanahmet</h2>
+            <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
           </div>
-          <a href="#">Takip et</a>
+          <a className="ml-auto text-blue-500" href="#">
+            Takip et
+          </a>
         </div>
-        <div>
-          <img src={ProfilePicture} />
-          <div>
-            <h2>ozcaanahmet</h2>
-            <p>Ahmet Özcan</p>
+        <div className="flex items-center ">
+          <img width={44} className=" rounded-full" src={ProfilePicture} />
+          <div className=" ml-3">
+            <h2 className=" font-semibold">ozcaanahmet</h2>
+            <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
           </div>
-          <a href="#">Takip et</a>
+          <a className="ml-auto text-blue-500" href="#">
+            Takip et
+          </a>
         </div>
-        <div>
-          <img src={ProfilePicture} />
-          <div>
-            <h2>ozcaanahmet</h2>
-            <p>Ahmet Özcan</p>
+        <div className="flex items-center ">
+          <img width={44} className=" rounded-full" src={ProfilePicture} />
+          <div className=" ml-3">
+            <h2 className=" font-semibold">ozcaanahmet</h2>
+            <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
           </div>
-          <a href="#">Takip et</a>
+          <a className="ml-auto text-blue-500" href="#">
+            Takip et
+          </a>
         </div>
-        <div>
-          <img src={ProfilePicture} />
-          <div>
-            <h2>ozcaanahmet</h2>
-            <p>Ahmet Özcan</p>
+        <div className="flex items-center ">
+          <img width={44} className=" rounded-full" src={ProfilePicture} />
+          <div className=" ml-3">
+            <h2 className=" font-semibold">ozcaanahmet</h2>
+            <p className="-mt-1 text-gray-600">Ahmet Özcan</p>
           </div>
-          <a href="#">Takip et</a>
+          <a className="ml-auto text-blue-500" href="#">
+            Takip et
+          </a>
         </div>
       </main>
-      <footer>
+      <footer className=" flex flex-col text-xs text-gray-500 gap-y-5 mt-5">
         <p>
           Hakkında · Yardım · Basın · API · Kariyer · Gizlilik · Şartlar ·
           Konumlar · Hesaplar · Hashtagler
